8e668cb556c5fec7c169ddea3a11ea980100c434
Ben Hall <Ben@Benhall.me.uk>
2010-09-24 15:50:46 +0100

    Refactoring. Redirect output back to stream

--- src/PersistentDlr/PersistentDlr/Dlr/DlrHost.cs
+++ src/PersistentDlr/PersistentDlr/Dlr/DlrHost.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using Microsoft.Scripting;
 using Microsoft.Scripting.Hosting;
 
@@ -11,21 +13,51 @@ namespace PersistentDlr.Dlr
 
         public DlrHost() {
             _scriptEngine = IronRuby.Ruby.CreateEngine();
+            
             _scriptScope = _scriptEngine.CreateScope();
         }
 
         public object Execute(string commandToExecute) {
-            try
-            {
-                ScriptSource s = _scriptEngine.CreateScriptSourceFromString(commandToExecute, SourceCodeKind.AutoDetect);
-                return s.Execute(_scriptScope);
+            try {
+                MemoryStream outputStream = RedirectOutputToStream();
+
+                ScriptSource scriptSource = _scriptEngine.CreateScriptSourceFromString(commandToExecute, SourceCodeKind.AutoDetect);
+                var resultOfExecution = scriptSource.Execute(_scriptScope);
+
+                if (!HasOutput(resultOfExecution))
+                    return ReadFromStream(outputStream);
+
+                return resultOfExecution;
             }
-            catch (System.Exception ex) {
-                var buffer = "IronRuby failed:" + ex.Message;
+
+            catch (Exception ex) {
+                var buffer = "IronRuby failed: " + ex.Message;
                 Console.WriteLine(buffer);
 
                 return buffer;
             }
+        }
+
+        private bool HasOutput(object execute) {
+            return execute != null && !string.IsNullOrEmpty(execute.ToString());
+        }
+
+        private MemoryStream RedirectOutputToStream() {
+            var memoryStream = new MemoryStream();
+            _scriptEngine.Runtime.IO.SetOutput(memoryStream, new StreamWriter(memoryStream));
+            _scriptEngine.Runtime.IO.SetErrorOutput(memoryStream, new StreamWriter(memoryStream));
+            return memoryStream;
+        }
+
+        private static string ReadFromStream(MemoryStream ms)
+        {
+            int length = (int)ms.Length;
+            Byte[] bytes = new Byte[length];
+
+            ms.Seek(0, SeekOrigin.Begin);
+            ms.Read(bytes, 0, (int)ms.Length);
+
+            return Encoding.GetEncoding("utf-8").GetString(bytes, 0, (int)ms.Length);
         }
     }
 }
